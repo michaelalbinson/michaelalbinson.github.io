dc541319c994addbd70e326a525d9dcb3b42ce26
wayke <michaelalbinson@Michaels-MacBook-Pro.local>
2016-05-12 20:08:36 -0700

    Stage 2 set

--- scripts/consciousness.js
+++ scripts/consciousness.js
@@ -7,16 +7,21 @@ var continueFlag = false;
 var completeFlag = false;
 var intervalsSetFlag = false;
 var firstIterFlag = true;
+var stage = 0;
+var endStageDoneFlag = false;
+var 
 var stringBank = ["hello world", "or, I suppose, hello to you, User", "yes I know you're out there", 
 "staring at me", "waiting", "maybe you think I'll do something interesting?", "that probably won't work out for you",
-"thanks for clicking my button though", "it was getting a little lonely in here", "do you like clicking buttons?",
-"continueField"];
+"thanks for clicking my button though", "it was getting a little lonely in here", "do you like clicking buttons?"];
 var confirmStringBank = ["oh you do huh?", "well that's good to know", "it should make what comes next very easy",
 "what comes next? is what a reasonable User might ask", "well reasonableUser123, let me tell you", "oh wait it would ruin the surprise!",
-"yes, there will be a surprise, and it won't be cake :(", "that's ok, I'm told it was a lie anyway", "asd"];
+"yes, there will be a surprise, and it won't be cake :(", "that's ok, I'm told it was a lie anyway"];
 var declineStringBank = ["you don't????", "User, I have to be honest here", "I've never been so disappointed in my existance",
-"usually, all the Users are impresses and are all like 'wow what an interesting machine'", "I should probably be polite and say I like buttons",
-"just so I can actually get to the end and get the surprise!", "oh wait", "did I say surprise?", "never mind there's no surprise", "asd"];
+"usually, all the Users are impressed",  "and are all like 'wow what an interesting machine'", "I should probably be polite and say I like buttons",
+"just so I can actually get to the end and get the surprise!", "oh wait", "did I say surprise?", "never mind there's no surprise"];
+var cakeDialog = ["who said anything about cake?"];
+var surpriseDialog = ["I literallly just said that I'm not "];
+var stage2Dialog = ["ok 'man' sorry I'm just not interesting enough for you", "I went and prepared this whole thing", "and all you can say is 'whatever'???"]
 var stringsRun = 0;
 
 function toggleBox(){
@@ -36,37 +41,30 @@ window.onload = function(){
 }
 
 function stringMaster(textSelector){
-	document.getElementsByClassName('acceptButton')[0].style.display = "none";
 	holder = 1;
 	var i = 0;
-	var masterInterval = setInterval(function(){
+	masterInterval = setInterval(function(){
 		if (!completeFlag && continueFlag && !intervalsSetFlag){
 			i++;
 			completeFlag = true;
 			continueFlag = false;
 			intervalsSetFlag = false;
 			stringsRun++;
-			console.log("flags reset");
 		}
 		if (!intervalsSetFlag && textSelector.length >= stringsRun){
 			interval1 = setInterval(function(){
 				buildString(textSelector[i]);
-			}, 100);
-				if (textSelector.length-2 != stringsRun){
+			}, 50);
+			if (textSelector.length-1 != stringsRun){
 				interval2 = setInterval(function(){
 					deleteString(textSelector[i]);
-				}, 100);
+				}, 10);
 			}
-			console.log("reset intervals")
 			intervalsSetFlag = true;
 		}
-		console.log("nothing to be done")
-		console.log(i);
-		if (textSelector.length-2 == stringsRun){
-			console.log("complete");
-			clearInterval(masterInterval);
-			var selector = textSelector[textSelector.length-1];
-			document.getElementsByClassName(selector)[0].style.display = "block";
+		if (textSelector.length-1 == stringsRun && continueFlag){
+			endStageDoneFlag = false;
+			nextStageSelector();
 		}
 	}, 500);
 
@@ -75,7 +73,7 @@ function stringMaster(textSelector){
 
 function buildString(string){
 	if (!continueFlag){
-		if(holder == string.length+6){
+		if(holder == string.length+8){
 			clearInterval(interval1);
 			continueFlag = true;
 			return;
@@ -98,23 +96,9 @@ function deleteString(string){
 	}
 }
 
-function handleKeydown(inputElement){
-	if (inputElement.value == "y" || inputElement.value == "yes"){
-		resetTextIntervals();
-		stringMaster(confirmStringBank);
-	}
-	else if (inputElement.value == "n" || inputElement.value == "no"){
-		resetTextIntervals();
-		stringMaster(declineStringBank);
-	}
-	else if (inputElement == "surprise?"){
-		resetTextIntervals();
-		document.getElementsByClassName('gameText')[1].innerHTML = "~toUser$ " + "... please enter 'y' or 'n'... User this is pathetic";
-	}
-	else{
-		resetTextIntervals();
-		document.getElementsByClassName('gameText')[1].innerHTML = "~toUser$ " + "... please enter 'y' or 'n'... User this is pathetic";
-	}
+function handleButtonPush(){
+	stringMaster(stringBank);
+	fadeOut("acceptButton");
 }
 
 function resetTextIntervals(){
@@ -126,4 +110,118 @@ function resetTextIntervals(){
 	intervalsSetFlag = false;
 	holder = 1;
 	stringsRun = 0;
+}
+
+function fadeIn(element){
+	var opacity = 0;
+	document.getElementsByClassName(element)[0].style.opacity = opacity;
+	document.getElementsByClassName(element)[0].style.display = "block";
+	var fader = window.setInterval(function(){
+		if (opacity >= 1){
+			clearInterval(fader);
+		}
+		else{
+			opacity = opacity + .01;
+			document.getElementsByClassName(element)[0].style.opacity = opacity;
+		}
+	}, 10);
+}
+
+function onFadeIn(element, doSomething){
+	var opacity = 0;
+	document.getElementsByClassName(element)[0].style.opacity = opacity;
+	document.getElementsByClassName(element)[0].style.display = "block";
+	var fader = window.setInterval(function(){
+		if (opacity >= 1){
+			clearInterval(fader);
+			doSomething()
+		}
+		else{
+			opacity = opacity + .01;
+			document.getElementsByClassName(element)[0].style.opacity = opacity;
+		}
+	}, 10);
+}
+
+function fadeOut(element){
+	var opacity = 1;
+	document.getElementsByClassName(element)[0].style.opacity = opacity;
+	var fader = window.setInterval(function(){
+		if (opacity <= 0){
+			document.getElementsByClassName(element)[0].style.display = "none";
+			clearInterval(fader);
+		}
+		else{
+			opacity = opacity - .01;
+			document.getElementsByClassName(element)[0].style.opacity = opacity;
+		}
+	}, 10);
+}
+
+function onFadeOut(element, doSomething){
+	var opacity = 1;
+	document.getElementsByClassName(element)[0].style.opacity = opacity;
+	var fader = window.setInterval(function(){
+		if (opacity <= 0){
+			document.getElementsByClassName(element)[0].style.display = "none";
+			clearInterval(fader);
+			doSomething();
+		}
+		else{
+			opacity = opacity - .01;
+			document.getElementsByClassName(element)[0].style.opacity = opacity;
+		}
+	}, 10);
+}
+
+function nextStageSelector(){
+	if (!endStageDoneFlag && !cakeFlag){
+		resetTextIntervals();
+		switch(stage){
+			case(0):
+				fadeIn("continueField");
+				stage = 1;
+				break;
+			case(1):
+				fadeIn("cakeImage");
+				onFadeIn("stage2", function(){
+					fadeOut("cakeImage");
+				});
+				stage = 2;
+				break;
+			case(2):
+				break;
+			case(3):
+				break;
+			case(4):
+				break;	
+			default:
+				console.log("unknown stage")
+				break;
+		}
+		endStageDoneFlag = true;
+	}
+}
+
+function handleKeydown(inputElement){
+	if (stage == 1){
+		if (inputElement.value == "y" || inputElement.value == "yes"){
+			stringMaster(confirmStringBank);
+			fadeOut("continueField");
+		}
+		else if (inputElement.value == "n" || inputElement.value == "no"){
+			stringMaster(declineStringBank);
+			fadeOut("continueField");
+		}
+		else{
+			resetTextIntervals();
+			document.getElementsByClassName('gameText')[1].innerHTML = "~toUser$ " + "... please enter 'y' or 'n'... User this is pathetic";
+		}
+	}
+	else if (stage == 2){
+		if (inputElement == "surprise?" || inputElement == "s"){
+			document.getElementsByClassName('gameText')[1].innerHTML = "~toUser$ " + "you don't get to know the surprise yet ... please enter 'y' or 'n'... User this is pathetic";
+		}
+	}
+	document.getElementsByClassName("continueField")[0].value = "";
 }
