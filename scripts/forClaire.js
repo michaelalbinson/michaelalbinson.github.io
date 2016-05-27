889bb8aba537dab7146ae077c10c117a8f93bca6
michaelalbinson <mikealbinson@gmail.com>
2016-05-26 21:28:24 -0700

    more claire

--- scripts/forClaire.js
+++ scripts/forClaire.js
@@ -3,20 +3,21 @@ var holder;
 var interval1;
 var interval2;
 var masterInterval;
+
 var continueFlag = false;
 var completeFlag = false;
 var intervalsSetFlag = false;
-var multiuseFlag = false;
+
 var stage = 0;
 var endStageDoneFlag = false;
 var cakeFlag = false;
 var annoyingCounter = 0;
 var stringsRun = 0;
-var branchTracker = "";
-var slowRenderSpeed = 100;
-var medRenderSpeed = 75;
 var fastRenderSpeed = 50;
 var devRenderSpeed = 1;
+
+var oneMessageRead = false;
+
 var timeBetweenCharRender = devRenderSpeed;
 var startupSpeak = ["hey you", "just wanted to give you a little something", "cause turns out it's expensive af to send things to Canada", "trust me I know",
 	"plus this is super fast", "but I figured I'd let you have a choice", "on what exactly you'd get to read", "so I made two", "your pick :)"];
@@ -25,7 +26,9 @@ var sassySpeak = ["good choice ;)", "so I heard that it was your birthday", "oh 
 	"eat a shitton of cake and get shitfaced", "cause that's what I would do", "plus maybe do other things", "like appreciate your family or whatever",
 	"cause they're probably like marginally important", "I totally don't wish I was there btw", "thanks for asking", "definitely don't miss you or whatever",
 	"definitely don't wish I was there right now to have some birthday fun with you", "who needs celebrations anyway, amirite?", "or amirite?",
-	"sitting on a boat is definitely waaaay more exciting", "(just remember you picked the sassy optiong here"];
+	"sitting on a boat is definitely waaaay more exciting", "(just remember you picked the sassy option here)", "so I got you a balloon", "I hope you like it",
+	"I worked like ridiculously hard on it", "anyway", "enjoy the fruits of being old enough to almost be trusted by the law",
+	"and I'll talk to you soon :)", "promise"];
 var cuteSpeak = ["better choice", "truth be told I ran out of ideas for the other one within 5 minutes", "plus I wanted to do this one more",
 	"so hey", "hope you have (had) a tremendous day", "filled with ridiculous frivolities and spending on alcohol", "I miss you", "I hope things have been going well",
 	"I know we haven't been talking as much as you probably hoped", "and I fell really bad about that", "work has been kinda eating my life a little",
@@ -35,7 +38,7 @@ var cuteSpeak = ["better choice", "truth be told I ran out of ideas for the othe
 	"like it's not even funny", "like maybe a little pathetic on my end", "but like, you know", "it's your special day", "I hope you have an amazing one",
 	"eat cake", "celebrate how you're not twenty yet and how scary that'll be", "and I can't wait to talk to you :)", "<3"];
 var prefixString = "/Michael/:";
-var postfixString = "~toClaire$"
+var postfixString = "~toClaire$";
 
 function stringRunnerManager(textSelector){
 	holder = 1;
@@ -75,8 +78,8 @@ function nextStageSelector(){
 				stage = 1;
 				break;
 			case(1):
-				document.getElementsByClassName('continueField')[0].value = "reset to see other message (you know you wanna)";
-				fadeIn("continueField");
+				document.getElementsByClassName('acceptButton')[0].value = "reset to see other message (you know you wanna)";
+				fadeIn("acceptButton");
 				break;
 			default:
 				console.log("unknown stage");
@@ -102,16 +105,22 @@ function handleButtonPush(){
 		fadeOut("acceptButton");
 		fadeOut('cakeImage');
 	}
-	else if (stage == 2){
-		resetToChoices();
+	else if (stage == 1){
+		if (oneMessageRead){
+			resetToChoices("your choice :)");
+			oneMessageRead = true;
+		}
+		else
+			resetToChoices("btw, there may or may not be something actually in the mail too... ;)");
 		fadeOut("acceptButton");
 		stage = 0;
+		
 	}
 }
 
 
-function resetToChoices(){
+function resetToChoices(stringToDisplay){
 	stage = 0;
 	clearInterval(masterInterval);
-	stringRunnerManager(startupSpeak);
+	stringRunnerManager([stringToDisplay]);
 }
