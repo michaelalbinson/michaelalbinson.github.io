4414922da241776f7dcc648d474c9c080fa39094
michaelalbinson <mikealbinson@gmail.com>
2016-05-26 21:36:25 -0700

    done w claire's stuff

--- scripts/forClaire.js
+++ scripts/forClaire.js
@@ -15,10 +15,9 @@ var annoyingCounter = 0;
 var stringsRun = 0;
 var fastRenderSpeed = 50;
 var devRenderSpeed = 1;
-
 var oneMessageRead = false;
+var timeBetweenCharRender = fastRenderSpeed;
 
-var timeBetweenCharRender = devRenderSpeed;
 var startupSpeak = ["hey you", "just wanted to give you a little something", "cause turns out it's expensive af to send things to Canada", "trust me I know",
 	"plus this is super fast", "but I figured I'd let you have a choice", "on what exactly you'd get to read", "so I made two", "your pick :)"];
 var sassySpeak = ["good choice ;)", "so I heard that it was your birthday", "oh wait sorry the day of celebration of south african land expropriation",
@@ -28,7 +27,7 @@ var sassySpeak = ["good choice ;)", "so I heard that it was your birthday", "oh 
 	"definitely don't wish I was there right now to have some birthday fun with you", "who needs celebrations anyway, amirite?", "or amirite?",
 	"sitting on a boat is definitely waaaay more exciting", "(just remember you picked the sassy option here)", "so I got you a balloon", "I hope you like it",
 	"I worked like ridiculously hard on it", "anyway", "enjoy the fruits of being old enough to almost be trusted by the law",
-	"and I'll talk to you soon :)", "promise"];
+	"and I'll talk to you soon :)", "promise ~~(*_*)~~~"];
 var cuteSpeak = ["better choice", "truth be told I ran out of ideas for the other one within 5 minutes", "plus I wanted to do this one more",
 	"so hey", "hope you have (had) a tremendous day", "filled with ridiculous frivolities and spending on alcohol", "I miss you", "I hope things have been going well",
 	"I know we haven't been talking as much as you probably hoped", "and I fell really bad about that", "work has been kinda eating my life a little",
@@ -79,7 +78,9 @@ function nextStageSelector(){
 				break;
 			case(1):
 				document.getElementsByClassName('acceptButton')[0].value = "reset to see other message (you know you wanna)";
+				document.getElementsByClassName('cakeImage')[0].src = "css/images/balloon.png";
 				fadeIn("acceptButton");
+				fadeIn('cakeImage');
 				break;
 			default:
 				console.log("unknown stage");
@@ -112,6 +113,7 @@ function handleButtonPush(){
 		}
 		else
 			resetToChoices("btw, there may or may not be something actually in the mail too... ;)");
+		fadeOut("cakeImage");
 		fadeOut("acceptButton");
 		stage = 0;
 		
