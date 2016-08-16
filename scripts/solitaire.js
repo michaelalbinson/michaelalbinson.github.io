6ffaf5ed0311121cd7b20a36ffb3d6b053316b23
michaelalbinson <mikealbinson@gmail.com>
2016-08-15 23:04:21 -0400

    error message finalized

--- scripts/solitaire.js
+++ scripts/solitaire.js
@@ -4,26 +4,22 @@ function GameManager() {
 	this.cards = document.getElementsByClassName('rowNum');
 	this.drawButton = document.getElementsByClassName('draw')[0];
 	this.gameBegun = false;
-	var interval = setInterval(makeSureWindowLargeEnough, 1000);
+	var domMan = new DOMManipulator();
+	var interval = setInterval(makeSureWindowLargeEnough, 500);
 	var rowManager = null;
-	var bodyHidden = false;
 
 	this.begin = function() {
-		for (card in this.cards){
-			setElementToImage(card);
-		}
-		enableOrDisableDrawButton(false);
 		rowManager = new RowManager();
+		domMan.enableOrDisableDrawButton(false);
 		daMan.gameBegun = true;
+	}
+
+	function makeSureWindowLargeEnough(){
+		domMan.makeSureWindowLargeEnough();
 	}
 
 	function setElementToImage(element) {
 		element.innerHTML = "";
-	}
-
-	function enableOrDisableDrawButton(enable){
-		var drawButton = document.getElementsByClassName('draw')[0];
-		drawButton.disabled = enable;
 	}
 
 	this.determinePosition = function(sender){
@@ -59,38 +55,8 @@ function GameManager() {
 		console.log("draw");
 	}
 
-	function makeSureWindowLargeEnough() {
-		if (window.innerWidth > 1280 && bodyHidden){
-			displayGameBody(bodyHidden);
-			bodyHidden = false;
-			return;
-		}
-		else if (window.innerWidth < 1280 && !bodyHidden){
-			displayGameBody(bodyHidden);
-			bodyHidden = true;
-		}
-	}
-
-	//Move to new class DOMManiputator or something
-	function displayGameBody(display) {
-		var gameBody = document.getElementsByClassName('game-body')[0];
-		var warningMsg = document.getElementsByClassName('warning')[0];
-		var placeholder = document.getElementsByClassName('placeholder')[0];
-		if (!gameBody || !warningMsg){
-			console.log('failed to get gameBody or warning body');
-			return;
-		}
-
-		if (display){ 
-			gameBody.style.display = "block";
-			warningMsg.style.display = "none";
-			placeholder.style.display = "none";
-		}
-		else{
-			gameBody.style.display = "none";
-			warningMsg.style.display = "block";
-			placeholder.style.display = "block";
-		}
+	this.newWindow = function(){
+		window.open("./solitaire.html", "", "width=1280, height=700")
 	}
 }
 
@@ -134,7 +100,7 @@ function Row(rowName){
 		return cardStack;
 	}
 
-	this.checkIfStackContains = function() {
+	this.checkIfStackContains = function(card) {
 
 	}
 
@@ -148,21 +114,21 @@ function DeckManager() {
 
 	//shuffles the deck using the Fischer-Yates shuffle
 	this.shuffle = function(deck) {
-  		var m = deck.length, t, i;
+		var m = deck.length, t, i;
 
   		// While there remain elements to shuffle…
   		while (m) {
 
     	// Pick a remaining element…
-    		i = Math.floor(Math.random() * m--);
+    	i = Math.floor(Math.random() * m--);
 
     		// And swap it with the current element.
     		t = deck[m];
     		deck[m] = deck[i];
     		deck[i] = t;
-  		}
-  		return deck;
-	}
+    	}
+    	return deck;
+    }
 
 	//initializes the deck ordered 1-52
 	function init() {
@@ -183,5 +149,82 @@ function DeckManager() {
 		this.deck.splice(0, 1);
 		console.log(this.deck);
 		return top;
+	}
+}
+
+function DOMManipulator() {
+	var drawButton = document.getElementsByClassName('draw')[0];
+	var gameBody = document.getElementsByClassName('game-body')[0];
+	var warningMsg = document.getElementsByClassName('warning')[0];
+	var placeholder = document.getElementsByClassName('placeholder')[0];
+	var bodyHidden = true;
+
+	this.enableOrDisableDrawButton = function(enable){
+		drawButton.disabled = enable;
+	}
+
+	this.makeSureWindowLargeEnough = function() {
+		if (window.innerWidth > 1280 && bodyHidden){
+			displayGameBody(bodyHidden);
+			bodyHidden = false;
+			return;
+		}
+		else if (window.innerWidth < 1280 && !bodyHidden){
+			displayGameBody(bodyHidden);
+			bodyHidden = true;
+		}
+	}
+
+	function displayGameBody(display) {
+		if (!gameBody || !warningMsg){
+			console.log('failed to get gameBody or warning body');
+			return;
+		}
+
+		if (display){ 
+			gameBody.style.display = "block";
+			warningMsg.style.display = "none";
+			placeholder.style.display = "none";
+		}
+		else{
+			gameBody.style.display = "none";
+			warningMsg.style.display = "block";
+			placeholder.style.display = "block";
+		}
+	}
+}
+
+//rawNumber must be from 1-52
+function Card(rawNumber) {
+	var rawNumber = rawNumber;
+	var suit;
+	var number;
+	var cardNames = ['A', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+	var suits = ['heart', 'spade', 'diamond', 'club'];
+
+	determineSuitAndNumber(rawNumber);
+
+	this.getSuit = function() {
+		return suit;
+	}
+
+	this.getNumber = function() {
+		return number;
+	}
+
+	function determineSuitAndNumber(rawNumber){
+		if (rawNumber < 14) //1-13
+			setSuitAndNumber(rawNumber, 0);
+		else if (rawNumber >= 14 && rawNumber < 27) //14-26
+			setSuitAndNumber(rawNumber-13, 1);
+		else if (rawNumber >= 27 && rawNumber < 40) //27-39
+			setSuitAndNumber(rawNumber-26, 2);
+		else //40-52
+			setSuitAndNumber(rawNumber-39, 3);
+	}
+
+	function setSuitAndNumber(cardNumber, suitNo){
+		suit = suits[suitNo];
+		number = cardNames[cardNumber-1]; //translate to 0-12 from 1-13
 	}
 }
