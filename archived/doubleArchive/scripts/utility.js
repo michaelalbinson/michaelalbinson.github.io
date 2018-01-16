e130c94c08a69652aba739884e690f0b9057bf22
Michael Albinson <mikealbinson@gmail.com>
2018-01-16 02:32:21 -0500

    fixing the archives cont

---
+++ archived/doubleArchive/scripts/utility.js
@@ -0,0 +1,132 @@
+function fadeIn(element){
+	if (document.getElementsByClassName(element)[0].getAttribute('fadedIn') == "true")
+		return;
+
+	var opacity = 0;
+	document.getElementsByClassName(element)[0].setAttribute("fadedIn", "true");
+	document.getElementsByClassName(element)[0].style.opacity = opacity;
+	document.getElementsByClassName(element)[0].style.display = "block";
+	var fader = window.setInterval(function(){
+		if (opacity >= .99){
+			clearInterval(fader);
+		}
+		else{
+			opacity = opacity + .01;
+			document.getElementsByClassName(element)[0].style.opacity = opacity;
+			fadingIn = true;
+		}
+	}, 10);
+}
+
+function onFadeIn(element, doSomething){
+	if (document.getElementsByClassName(element)[0].getAttribute('fadedIn') == "true")
+		return;
+
+	document.getElementsByClassName(element)[0].setAttribute("fadedIn", "true");
+	var opacity = 0;
+	document.getElementsByClassName(element)[0].style.opacity = opacity;
+	document.getElementsByClassName(element)[0].style.display = "block";
+	var fader = window.setInterval(function(){
+		if (opacity >= 1){
+			clearInterval(fader);
+			doSomething()
+		}
+		else{
+			opacity = opacity + .01;
+			document.getElementsByClassName(element)[0].style.opacity = opacity;
+		}
+	}, 10);
+}
+
+function fadeOut(element){
+	if (document.getElementsByClassName(element)[0].getAttribute('fadedIn') == "false")
+		return;
+
+	document.getElementsByClassName(element)[0].setAttribute("fadedIn", "false");
+	var opacity = 1;
+	document.getElementsByClassName(element)[0].style.opacity = opacity;
+	var fader = window.setInterval(function(){
+		if (opacity <= 0){
+			document.getElementsByClassName(element)[0].style.display = "none";
+			clearInterval(fader);
+		}
+		else{
+			opacity = opacity - .01;
+			document.getElementsByClassName(element)[0].style.opacity = opacity;
+		}
+	}, 10);
+}
+
+function onFadeOut(element, doSomething){
+	if (document.getElementsByClassName(element)[0].getAttribute('fadedIn') == "false")
+		return;
+
+	document.getElementsByClassName(element)[0].setAttribute("fadedIn", "false");
+	fadingOut = true;
+	var opacity = 1;
+	document.getElementsByClassName(element)[0].style.opacity = opacity;
+	var fader = window.setInterval(function(){
+		if (opacity <= 0){
+			document.getElementsByClassName(element)[0].style.display = "none";
+			clearInterval(fader);
+			doSomething();
+		}
+		else{
+			opacity = opacity - .01;
+			document.getElementsByClassName(element)[0].style.opacity = opacity;
+		}
+	}, 10);
+}
+
+//function to fade out text and shrink the text as it fades
+//the font size of the element will be reset after it is hidden,
+//in case it will be displayed again
+//element: any element with text you wish to shrink
+//initFontSize: initial font size in em (should be in em, otherwise weird things will happen)
+function fadeOutWithShrinkingText(element, initFontSize){
+	if (document.getElementsByClassName(element)[0].getAttribute('fadedIn') == "false")
+		return;
+
+	document.getElementsByClassName(element)[0].setAttribute("fadedIn", "false");
+	var opacity = 1;
+	var iFS = initFontSize;
+	document.getElementsByClassName(element)[0].style.opacity = opacity;
+	var fader = window.setInterval(function(){
+		if (opacity <= 0){
+			document.getElementsByClassName(element)[0].style.display = "none";
+			document.getElementsByClassName(element)[0].style.fontSize = initFontSize.toString() + 'em';
+			clearInterval(fader);
+		}
+		else{
+			opacity = opacity - .01;
+			iFS -= .01;
+			document.getElementsByClassName(element)[0].style.opacity = opacity;
+			document.getElementsByClassName(element)[0].style.fontSize = iFS.toString() + 'em';
+		}
+	}, 20);
+}
+
+function buildString(string){
+	if (!continueFlag){
+		if(holder == string.length+8){
+			clearInterval(interval1);
+			continueFlag = true;
+			return;
+		}
+		document.getElementsByClassName('gameText')[0].innerHTML = prefixString + " " + postfixString + " "  + string.substring(0, holder);
+		holder++;
+	}
+}
+
+function deleteString(string){
+	if (continueFlag){
+		if (holder == 0){
+			clearTimeout(interval2);
+			completeFlag = false;
+			intervalsSetFlag = false;
+		}
+
+		document.getElementsByClassName('gameText')[0].innerHTML = prefixString + " " + postfixString + " " + string.substring(0, holder);
+		holder--;
+	}
+}
