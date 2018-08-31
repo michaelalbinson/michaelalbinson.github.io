e536cc9616ee67e7659412c1aba1d76d97c81faa
michael.albinson <michael.albinson@servicenow.com>
2018-08-30 20:43:06 -0700

    port consciousness

---
+++ games/consciousness/js/preferenceTableFunction.js
@@ -0,0 +1,36 @@
+function switchSections(toSection){
+	document.getElementsByClassName(rightForm)[0].style.display = 'none';
+	document.getElementsByClassName(centerForm)[0].style.display = 'none';
+	document.getElementsByClassName(leftForm)[0].style.display = 'none';
+
+	document.getElementsByClassName('leftButton')[0].style.border = 'none';
+	document.getElementsByClassName('centerButton')[0].style.border = 'none';
+	document.getElementsByClassName('rightButton')[0].style.border = 'none';
+
+	document.getElementsByClassName('leftButton')[0].style.padding = '3px';
+	document.getElementsByClassName('centerButton')[0].style.padding = '3px';
+	document.getElementsByClassName('rightButton')[0].style.padding = '3px';
+
+	switch(toSection){
+		case 'left':
+			document.getElementsByClassName(rightForm)[0].style.display = 'block';
+			document.getElementsByClassName('leftButton')[0].style.border = 'inset';
+			document.getElementsByClassName('leftButton')[0].style.padding = '0';
+			document.getElementsByClassName('optionHeader')[0].innerHTML = leftHeader;
+			break;
+		case 'center':
+			document.getElementsByClassName(centerForm)[0].style.display = 'block';
+			document.getElementsByClassName('centerButton')[0].style.border = 'inset';
+			document.getElementsByClassName('centerButton')[0].style.padding = '0';
+			document.getElementsByClassName('optionHeader')[0].innerHTML = centerHeader;
+			break;
+		case 'right':
+			document.getElementsByClassName(leftForm)[0].style.display = 'block';
+			document.getElementsByClassName('rightButton')[0].style.border = 'inset';
+			document.getElementsByClassName('rightButton')[0].style.padding = '0';
+			document.getElementsByClassName('optionHeader')[0].innerHTML = rightHeader;
+			break;
+		default:
+			console.log('uncaught exception: unable to switch sections');
+	}
+}
